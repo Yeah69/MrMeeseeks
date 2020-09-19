2ee22eb132efb4a0c6be022dba175afdc0e4fc06
Dima Enns <dieter.enns@gmail.com>
2020-09-19 15:42:05 +0200

    Added RxCommand

---
+++ MrMeeseeks.Windows/RxCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Windows.Input;
+
+namespace MrMeeseeks.Windows
+{
+    public interface IRxCommand : ICommand, IDisposable
+    {
+        IObservable<object> Observe { get; }
+
+        IObservable<T> ObserveOfType<T>() => Observe.OfType<T>();
+
+        IObservable<T> ObserveCast<T>() => Observe.Cast<T>();
+    }
+
+    public class RxCommand : IRxCommand
+    {
+        private readonly Subject<object> _commandTriggering = new Subject<object>();
+        private readonly IDisposable _canExecuteSubscription;
+
+        private bool _canExecute;
+        
+        public RxCommand(
+            bool initialCanExecute = true)
+        {
+            _canExecuteSubscription = Disposable.Empty;
+            _canExecute = initialCanExecute;
+        }
+        
+        public RxCommand(
+            IObservable<bool> observeCanExecute,
+            bool initialCanExecute = true)
+        {
+            _canExecuteSubscription = observeCanExecute
+                .Subscribe(b =>
+                {
+                    _canExecute = b;
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                });
+            _canExecute = initialCanExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute;
+
+        public void Execute(object parameter) => _commandTriggering.OnNext(parameter);
+
+        public event EventHandler? CanExecuteChanged;
+        public void Dispose()
+        {
+            _commandTriggering.Dispose();
+            _canExecuteSubscription.Dispose();
+        }
+
+        public IObservable<object> Observe => _commandTriggering.AsObservable();
+    }
+}
--- MrMeeseeks/Extensions/StringExtensions.cs
+++ MrMeeseeks/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-using System.Runtime.CompilerServices;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace MrMeeseeks.Extensions
 {
@@ -8,5 +9,21 @@ namespace MrMeeseeks.Extensions
         public static bool IsNullOrWhitespace(this string? @this) => string.IsNullOrWhiteSpace(@this);
         public static bool IsEmpty(this string @this) => string.IsNullOrEmpty(@this);
         public static bool IsWhitespace(this string @this) => string.IsNullOrWhiteSpace(@this);
+    }
+    
+    public static class StringRemoveIllegalFilePathCharactersExtension
+    {
+        static StringRemoveIllegalFilePathCharactersExtension()
+        {
+            MatchAllIllegalFilePathCharacters = new Regex(
+                $"[{Regex.Escape(new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars()))}]");
+        }
+
+        private static readonly Regex MatchAllIllegalFilePathCharacters;
+
+        public static string RemoveIllegalFilePathCharacters(this string @this)
+        {
+            return MatchAllIllegalFilePathCharacters.Replace(@this, "");
+        }
     }
 }
