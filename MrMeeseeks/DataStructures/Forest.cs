1432d88ef796ca8ef3927c039886c5a18a5a86f8
Dima Enns <dieter.enns@gmail.com>
2020-11-08 11:59:51 +0100

    Forest adjustment

--- MrMeeseeks/DataStructures/Forest.cs
+++ MrMeeseeks/DataStructures/Forest.cs
@@ -7,11 +7,19 @@ namespace MrMeeseeks.DataStructures
 {
     public class Forest<T> : TreeBase<T>
     {
+        public static Forest<T> CreateFromTrees(IReadOnlyList<Tree<T>> trees) => 
+            new Forest<T>(trees);
+        
         public IReadOnlyList<Tree<T>> Trees { get; }
 
         public Forest()
         {
             Trees = Children.ToReadOnlyList();
+        }
+
+        private Forest(IReadOnlyList<Tree<T>> trees)
+        {
+            Trees = trees;
         }
 
         public Forest<TResult> Select<TResult>(Func<T, IEnumerable<TResult>, TResult> resultSelector)
