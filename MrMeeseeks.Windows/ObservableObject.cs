7e993a4ae51efad026abc494c87c33f8ab12474f
Dima Enns <dieter.enns@gmail.com>
2021-01-08 22:32:28 +0100

    Notification escalation

--- MrMeeseeks.Windows/ObservableObject.cs
+++ MrMeeseeks.Windows/ObservableObject.cs
@@ -1,5 +1,9 @@
+using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
+using MrMeeseeks.Reactive.Extensions;
 
 namespace MrMeeseeks.Windows
 {
@@ -9,17 +13,22 @@ namespace MrMeeseeks.Windows
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
+        protected IDisposable EscalateNotifications(
+            IObservableObject observableObject,
+            params (string Source, string Target)[] propertyNameMapping) =>
+            propertyNameMapping
+                .Select(pair => observableObject.ObservePropertyChanged(pair.Source).Select(_ => pair.Target))
+                .Merge()
+                .Subscribe(OnPropertyChanged);
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        
 
         protected void OnPropertyChanged(params string[] propertyNames)
         {
             foreach (string propertyName in propertyNames)
-            {
                 OnPropertyChanged(propertyName);
-            }
         }
 
         protected void SetIfChangedAndRaise<T>(ref T field, T value,[CallerMemberName] string? propertyName = null)
